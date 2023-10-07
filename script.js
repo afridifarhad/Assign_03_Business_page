4d5d80810a42f39674e85ad8a0e3af1d11ce0022
Your Name <you@example.com>
2023-10-06 23:14:18 -0800

    I have complete my assignment it is a  GYM fronted website which have four pages

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the current URL
+var currentUrl = window.location.href;
+
+// Find all <a> elements in the navigation menu
+var navLinks = document.querySelectorAll("nav a");
+
+// Loop through the links and check if the link's href matches the current URL
+for (var i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].href === currentUrl) {
+        // If there's a match, add a "highlight" class to the link
+        navLinks[i].classList.add("highlight");
+        break; // Exit the loop once a match is found
+    }
+}
